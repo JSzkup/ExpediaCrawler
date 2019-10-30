0a6c5536519bcfd8e4b131fcdc8ddf3b54d5e0c2
Jszkup <jszkup101@gmail.com>
2019-10-30 16:43:39 -0400

    added gitignore/README, structured data collection

---
+++ README.txt
@@ -0,0 +1,7 @@
+a textfile mist be created named email.py in the main directory for this to work.
+
+email.py will be in this format:
+
+# email credentials
+username = 'XXXXX@YYYYYYY.com'
+password = 'XXXXXXXXXXX'
--- main.py
+++ main.py
@@ -23,12 +23,15 @@ return_ticket = "//label[@id='flight-type-roundtrip-label-hp-flight']"
 one_way_ticket = "//label[@id='flight-type-one-way-label-hp-flight']"
 multi_ticket = "//label[@id='flight-type-multi-dest-label-hp-flight']"
 
+# Panda dataframe to hold data
+df = pd.DataFrame()
+
 
 def ticket_chooser(ticket):  # Looks for the defined ticket types
     try:
         ticket_type = browser.find_element_by_xpath(ticket)
         ticket_type.click()
-    except Exception as e:
+    except Exception as i:
         pass
 
 
@@ -81,9 +84,95 @@ def return_date_chooser(month, day, year):  # Choose ARRIVAL date
     return_date_button.send_keys(month + '/' + day + '/' + year)
 
 
-def search():
+def search():  # Clicks the search button
     search = browser.find_element_by_xpath(
         "//button[@class='btn-primary btn-action gcw-submit']")
     search.click()
+    # Long delay, search loads slowly
     time.sleep(15)
+    # tells user when things are searched properly
     print('Results ready!')
+
+
+def compile_data():
+    # variables to store search results
+    global df
+    global dep_times_list
+    global arr_times_list
+    global airlines_list
+    global price_list
+    global durations_list
+    global stops_list
+    global layovers_list
+
+    # departure times
+    dep_times = browser.find_elements_by_xpath(
+        "//span[@data-test-id='departure-time']")
+    dep_times_list = [value.text for value in dep_times]
+
+    # arrival times
+    arr_times = browser.find_elements_by_xpath(
+        "//span[@data-test-id='arrival-time']")
+    arr_times_list = [value.text for value in arr_times]
+
+    # airline name
+    airlines = browser.find_elements_by_xpath(
+        "//span[@data-test-id='airline-name']")
+    airlines_list = [value.text for value in airlines]
+
+    # prices
+    prices = browser.find_elements_by_xpath(
+        "//span[@data-test-id='listing-price-dollars']")
+    # TODO check here for errors
+    price_list = [value.text.split('')[1] for value in prices]
+
+    # durations
+    durations = browser.find_elements_by_xpath(
+        "//span[@data-test-id='duration']")
+    durations_list = [value.text for value in durations]
+
+    # stops
+    stops = browser.find_elements_by_xpath("//span[@class='number-stops']")
+    stops_list = [value.text for value in stops]
+
+    # layovers
+    layovers = browser.find_elements_by_xpath(
+        "//span[@data-test-id='layover-airport-stops']")
+    layovers_list = [value.text for value in layovers]
+    # renaming price for every iteration
+    now = datetime.datetime.now()
+    current_date = (str(now.year) + '-' + str(now.month) + '-' + str(now.day))
+    current_time = (str(now.hour) + ':' + str(now.minute))
+    current_price = 'price' + '(' + current_date + '---' + current_time + ')'
+
+    # Creates a list of data for each variable
+    for i in range(len(dep_times_list)):
+        try:
+            df.loc[i, 'departure_time'] = dep_times_list[i]
+        except Exception as i:
+            pass
+        try:
+            df.loc[i, 'arrival_time'] = arr_times_list[i]
+        except Exception as i:
+            pass
+        try:
+            df.loc[i, 'airline'] = airlines_list[i]
+        except Exception as i:
+            pass
+        try:
+            df.loc[i, 'duration'] = durations_list[i]
+        except Exception as i:
+            pass
+        try:
+            df.loc[i, 'stops'] = stops_list[i]
+        except Exception as i:
+            pass
+        try:
+            df.loc[i, 'layovers'] = layovers_list[i]
+        except Exception as i:
+            pass
+        try:
+            df.loc[i, str(current_price)] = price_list[i]
+        except Exception as i:
+            pass
+    print('Excel Sheet Created!')
